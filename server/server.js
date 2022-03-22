206975317cfbff072d2f1805b01b0b53e9dde246
Lyle Jensen <lyle.jensen95@gmail.com>
2022-03-22 16:15:27 -0600

    added s3 upload logic to server.js

--- server/server.js
+++ server/server.js
@@ -4,22 +4,48 @@ const http = require("http");
 const server = http.createServer(app);
 const PORT = 3000;
 const mongoose = require("mongoose");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
 
-const homeRouter = require('./routes/routes');
-const mediaRouter = require('./routes/mediaRoutes');
-const userRouter = require('./routes/userRoutes');
+const homeRouter = require("./routes/routes");
+const mediaRouter = require("./routes/mediaRoutes");
+const userRouter = require("./routes/userRoutes");
+const { rejects } = require("assert");
+const { resolve } = require("path");
 
 require("dotenv").config();
 
-app.use('/', homeRouter);
-app.use('/media', mediaRouter);
-app.use('/user', userRouter);
+app.use("/", homeRouter);
+app.use("/media", mediaRouter);
+app.use("/user", userRouter);
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true })
   .then(console.log("Mongo connected"))
   .catch((err) => console.error(err));
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// });
+
+// const file = "./sample_video.mp4";
+// const fileContent = fs.createReadStream(file);
+
+// const params = {
+//   Bucket: process.env.AWS_BUCKET_NAME,
+//   Key: path.basename(file),
+//   Body: fileContent,
+// };
+
+// s3.upload(params, (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(`File uploaded successfully. ${data}`);
+// });
